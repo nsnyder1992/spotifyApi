842c46513752450e74bebc6d7dd06bdce9e6e333
Nick Snyder <nsnyder1992@gmail.com>
2020-12-03 17:48:11 -0500

    Auth is now working

--- assets/spotifyApi.js
+++ assets/spotifyApi.js
@@ -1,19 +1,57 @@
 $(document).ready(function () {
-  //spotify api
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const baseURL = "https://accounts.spotify.com/authorize";
-  const clientId = "?client_id=60c69366ae1e45d1adbfb0614a57e993";
-  const responseType = "&response_type=token";
-  const redirectUri = "https:%2F%2nsnyder1992.github.io%2spotifyApi";
-  const scope = "&scope=user-read-private%20user-read-email";
-  const state = "&state=123";
+  //spotify api button
   const spotifyApi = $("#spotify");
 
+  const authEndpoint = "https://accounts.spotify.com/authorize";
+
+  // Replace with your app's client ID, redirect URI and desired scopes
+  // const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const clientId = "60c69366ae1e45d1adbfb0614a57e993";
+  const redirectUri = "https://nsnyder1992.github.io/spotifyApi/";
+  const scopes = ["user-top-read"];
+
+  // Get the hash of the url
+  const hash = window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce(function (initial, item) {
+      if (item) {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+    }, {});
+  window.location.hash = "";
   spotifyApi.click(() => getSpotifyData());
 
-  async function getSpotifyData() {
-    await fetch(
-      proxyUrl + baseURL + clientId + responseType + redirectUri + scope + state
-    ).then((res) => console.log(res));
+  // Set token
+  let _token = hash.access_token;
+
+  function getSpotifyData() {
+    // If there is no token, redirect to Spotify authorization
+    if (!_token) {
+      window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+        "%20"
+      )}&response_type=token&show_dialog=true`;
+    } else {
+      console.log(_token);
+    }
+
+    // Make a call using the token
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/top/artists",
+      type: "GET",
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("Authorization", "Bearer " + _token);
+      },
+      success: function (data) {
+        console.log(data);
+        // Do something with the returned data
+        data.items.map(function (artist) {
+          let item = $("<li>" + artist.name + "</li>");
+          item.appendTo($("#spotify-insert"));
+        });
+      },
+    });
   }
 });
