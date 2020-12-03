60017a4ed422cc3ab1df7bc3cb42522bdbd0472c
Nick Snyder <nsnyder1992@gmail.com>
2020-12-03 16:46:41 -0500

    updated redirectUri

--- assets/spotifyApi.js
+++ assets/spotifyApi.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   const baseURL = "https://accounts.spotify.com/authorize";
   const clientId = "?client_id=60c69366ae1e45d1adbfb0614a57e993";
   const responseType = "&response_type=token";
-  const redirectUri = "https://nsnyder1992.github.io/portfolio/";
+  const redirectUri = "https://nsnyder1992.github.io/spotifyApi/";
   const spotifyApi = $("#spotify");
 
   spotifyApi.click(() => getSpotifyData());
