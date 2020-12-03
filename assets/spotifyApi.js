c277740b012b22290cf270918eec2ab51750843b
Nick Snyder <nsnyder1992@gmail.com>
2020-12-03 16:17:06 -0500

    first commit

---
+++ assets/spotifyApi.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  //spotify api
+  const baseURL = "https://accounts.spotify.com/authorize";
+  const clientId = "?client_id=60c69366ae1e45d1adbfb0614a57e993";
+  const responseType = "&response_type=token";
+  const redirectUri = "https://nsnyder1992.github.io/portfolio/";
+  const spotifyApi = $("#spotify");
+
+  spotifyApi.click(() => getSpotifyData());
+
+  async function getSpotifyData() {
+    await fetch(baseURL + clientId + responseType + redirectUri).then((res) =>
+      console.log(res)
+    );
+  }
+});
